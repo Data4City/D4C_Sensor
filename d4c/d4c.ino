d712d090bc0e3a042e17a28f735c49400b59627e
Dan Amador <danamadorpe@gmail.com>
2018-12-03 17:13:43 +0100

    Basic sensor input achieved

--- d4c/d4c.ino
+++ d4c/d4c.ino
@@ -1,13 +1,26 @@
-// the setup function runs once when you press reset or power the board
+
+#include <SenseBoxMCU.h>
+
+HDC1080 bmp;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+ 
+  if (!bmp.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+
+  }
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(300);                       
+  
+float  tempBaro = bmp.getTemperature();
+Serial.println(tempBaro);
+//    pressure = BMP.readPressure()/100;
+  //  altitude = BMP.readAltitude(1013.25); //1013.25 = sea level pressure
 }
