3d2aaadd16d98c20149e9ae478bbf7b693f9c31b
Dan Amador <danamadorpe@gmail.com>
2019-01-14 21:33:46 +0100

    Started update values method

--- rasp/Helpers/plugged_sensor.py
+++ rasp/Helpers/plugged_sensor.py
@@ -1,4 +1,6 @@
 import importlib, logging
+from datetime import datetime
+
 
 class PluggedSensor():
     def __init__(self, sensor, i2c):
@@ -12,8 +14,39 @@ class PluggedSensor():
 
         self.name = sensor["name"]
         self.model = sensor["model"]
-        self.values = get_sensor_lambdas(sensor["data"])
+        self.sensor_data = self.get_sensor_lambdas(sensor["data"])
 
-    def get_sensor_lambdas(data):
+    
+    def get_sensor_lambdas(self, data):
+        functions = []
         for unit in data:
-            %TODO CREATE THIS FUNCTION
+            currLambda = getattr(self.__sensor__, unit["function"])
+            current = {
+                'lambda': currLambda,
+                'data': SensorData(once = unit["once"], last_value=currLambda()),
+            }
+        return functions
+
+    def update_sensors(self):
+        for sensor_data in self.sensor_data:
+            data = sensor_data["data"]
+            if data.once:
+                continue
+            elif (data.last_checked - datetime.now).total_seconds() >= data.check_every:
+                curr_data.update(sensor_data["lambda"]())
+
+class SensorData():
+    def __init__(self, once = False, unit, last_value):
+        self.last_checked =  datetime.now()
+        self.once =  data["once"]
+        self.uploaded_to_server = false
+        self.last_value = last_value
+        self.units  = unit #SI unit that measures the value given
+    
+    def update(self, new_data):
+        #Update and add to queue if still not in server
+        self.last_value = new_data
+
+    
+    def __str__(self):
+        return ""
