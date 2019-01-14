c96dbb0c159e2d6c3b1d076ddb58fe9c3b5db882
Dan Amador <danamadorpe@gmail.com>
2019-01-14 20:53:24 +0100

    Retrieve and create sensors and logging

---
+++ rasp/main.py
@@ -0,0 +1,27 @@
+import argparse, json, logging
+from .Helpers import plugged_sensor
+currentSensors = []
+
+def initialize_sensors(sensorList):
+    for sensor in sensorList:
+            global currentSensors
+            if(sensor["type"] == "i2c"):
+                currentSensors.append(PluggedSensor(sensor))
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler('sensor.log')
+    handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+    parser = argparse.ArgumentParser(description="Choose parameters to be used with the sensor box")
+    parser.add_argument("sensors", metavar='j', help="Choose json file with the description of the available sensors")
+    parser.add_argument("debug", metavar='d', help="Choose to use dummy data")
+    args = parser.parse_args()
+    sensor_list = json.loads(args.sensors["sensors"])
+    initialize_sensors(sensor_list)
