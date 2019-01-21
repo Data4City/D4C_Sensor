62cc599d2f5199bdcde26436af83b3ebbc4a34a4
Dan Amador <danamadorpe@gmail.com>
2019-01-21 16:03:53 +0100

    Started working with the microphone

--- rasp/Helpers/microphone.py
+++ rasp/Helpers/microphone.py
@@ -1,11 +1,17 @@
 import sounddevice as sd
 import numpy as np
+    
+class MicrophoneSensor():
 
-duration = 10  # seconds
+    def __init__(self, test_time: int = 10):
+        self.test_time = test_time
+        with sd.Stream(callback=self.print_sound):
+            sd.sleep(test_time * 1000)
+        
+    def print_sound(self, indata, outdata, frames, time, status):
+        volume_norm = np.linalg.norm(indata)*10
+        print("|" * int(volume_norm))
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata)*10
-    print "|" * int(volume_norm)
 
-with sd.Stream(callback=print_sound):
-    sd.sleep(duration * 1000)
+if __name__ == "__main__":
+    mic = MicrophoneSensor()
