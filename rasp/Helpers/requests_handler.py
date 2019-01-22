cf99ad87543f78836f8bb6f12c846389e6dce3e4
Dan Amador <danamadorpe@gmail.com>
2019-01-22 19:10:47 +0100

    Modified namespace and started emitting updates

--- rasp/Helpers/requests_handler.py
+++ rasp/Helpers/requests_handler.py
@@ -1,10 +1,13 @@
 import requests
 from Helpers import plugged_sensor
+from FlaskHelpers import flask_helper as fh
 from Helpers import redis_helper as rh
 from rq.decorators import job
 
 @job('post', connection=rh.redis_server, timeout=5)
-def post_sensor(data: dict):
+def post_sensor(data: dict, socket_emit: bool = False):
     #TODO Change path
     print(data)
     requests.post("http://localhost:8888", data)
+    if socket_emit:
+        fh.publish_message("update", data)
