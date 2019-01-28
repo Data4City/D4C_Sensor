218d09273531c8e13ee72c97a083c7b68117bddf
Dan Amador <danamadorpe@gmail.com>
2019-01-28 17:55:39 +0100

    Small refactor in check every and added unique id to each rasp using the serial number as id

--- rasp/raspberry_handler.py
+++ rasp/raspberry_handler.py
@@ -2,15 +2,32 @@ import busio, board, asyncio
 from Helpers import plugged_sensor, requests_handler
 from threading import Thread
 
+__current_plugged_sensors = []
+__serial_num = None
 
-current_plugged_sensors = []
+def get_serial():
+  # Extract serial from cpuinfo file
+    if not __serial_num or __serial_num == "ERROR000000000":
+        __serial_num = "0000000000000000"
+        try:
+            f = open('/proc/cpuinfo','r')
+                for line in f:
+                    if line[0:6]=='Serial':
+                        __serial_num = line[10:26]
+                f.close()
+        except:
+            logger = logging.getLogger(__name__)
+            logger.error("Serial number not found")
+            __serial_num = "ERROR000000000"
+        return __serial_num
 
+  
 def initialize_sensors(sensorList):
     i2c = busio.I2C(board.SCL, board.SDA)
     for sensor in sensorList["sensors"]:
-            global current_plugged_sensors
+            global __current_plugged_sensors
             if(sensor["type"] == "i2c"):
-                current_plugged_sensors.append(plugged_sensor.PluggedSensor(sensor, i2c))
+                __current_plugged_sensors.append(plugged_sensor.PluggedSensor(sensor, i2c))
 
 def start_sense_loop():
     sensor_loop = asyncio.new_event_loop()
@@ -20,16 +37,18 @@ def start_sense_loop():
 
 async def sense():
     while True:
-        for sensor in current_plugged_sensors:
+        for sensor in __current_plugged_sensors:
             if sensor.update_sensors():
                 if sensor.type == "i2c":
                     for i,data in enumerate(sensor.sensor_data):
                         if not data.enqueued:
-                            requests_handler.post_sensor.delay(sensor.__post_data__(i))
+                            requests_handler.post_sensor.delay(sensor.__post_data__(i), get_serial())
                             data.enqueued = True
                 elif sensor.type == "mic":
                     print("Microphone check")
         await asyncio.sleep(10)
+
+
 
 
 
