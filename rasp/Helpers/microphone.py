900b9c8e660b39865f6bebe1ac439573fac88d3f
Dan Amador <danamadorpe@gmail.com>
2019-02-04 13:48:24 +0100

    Microphone basic look should be working

--- rasp/Helpers/microphone.py
+++ rasp/Helpers/microphone.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 import numpy as np
-import asyncio    
+import asyncio, time, queue
 class MicrophoneSensor():
 
     def __init__(self, window: int = 1500):
@@ -8,10 +8,12 @@ class MicrophoneSensor():
         self.samples = np.empty(0)
         self.window = window
         self.mean = 0.
-
+        self.recording = False 
+        self.record_start = None
+        self.record_queue = queue.Queue()
+        self.last_loud_noise = None
         loop = asyncio.get_event_loop()
         with sd.InputStream(callback=self.callback):
-
             while(True):
                 sd.sleep(10)
                 if(len(self.samples) >= self.window):
@@ -21,17 +23,29 @@ class MicrophoneSensor():
     
 
 
-    async def running_mean(self,x, N):
+    async def running_mean(self,x, N) -> float:
         cumsum = np.cumsum(np.insert(x, 0, 0)) 
         return np.mean((cumsum[N:] - cumsum[:-N]) / float(N))
     
+
+    def is_loud(self, volume):
+        return volume > self.mean *3: #TODO Figure something smarter than this
+ 
     def callback(self, indata, frames, time, status):
         volume_norm = np.linalg.norm(indata)*10
         self.samples = np.append(self.samples,volume_norm)
 
-        #print("|" * int(volume_norm))
+        if(is_loud(volume_norm)):
+            if(not self.recording):
+                self.recording = True
+                self.record_start = time.time()
+        
+        if(recording):
+            q.put(indata.copy())
+
+        if(time.time() - self.record_start > 5 ): #Make sure the clip is at least 5 seconds long
+        #print("|" * int(volume_norm))  
 
 
 if __name__ == "__main__":
     mic = MicrophoneSensor()
-    self.window = window
