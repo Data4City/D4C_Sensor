b405402bf9f6a06293ee740bf64e38e2475331b0
Dan Amador <danamadorpe@gmail.com>
2019-02-08 19:15:41 +0100

    Modified schema

--- api/models.py
+++ api/models.py
@@ -1,32 +1,40 @@
 from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Box(Base):
     __tablename__ = "box"
-    serial = Column('serial', String(16), primary_key=True)
+    #TODO Add geolocation
+    id = Column('id', Integer, primary_key=True)
+    serial = Column('serial', String(16))
+    sensors_used = relationship('Sensor')
+    measurement_data = relationship('Value')
 
-class Sensors(Base):
+class Sensor(Base):
     __tablename__ = "sensor"
     id = Column('id', Integer, primary_key=True)
     name = Column("name",String(40))
     model = Column("model", String(40))
+    box_id = Column(Integer, ForeignKey('box.id'))
+    measurements = relationship('Measurement')
 
-class Unit(Base):
-    __tablename__ = "unit"
-    symbol = Column('symbol', String(5), primary_key=True)
-    name = Column('name', String(30))
-
-class Measurement(Base):
+class Measurement(Base):    
     __tablename__ = "measurement"
     id = Column('id', Integer, primary_key=True)
-    value = Column("value", Float)
-    timestamp = Column("timestamp", DateTime(timezone=True))
-    sensor_id = Column(Integer, ForeignKey('sensor.id'), nullable= False)
-    unit_id = Column(Integer, ForeignKey('unit.symbol'), nullable= False)
-    box_serial = Column(Integer, ForeignKey('box.serial'), nullable= False)
+    used_by = Column(Integer, ForeignKey('sensor.id'), nullable= False)
+    symbol = Column('symbol', String(5))
+    name = Column('name', String(30))
 
+class Value(Base):
+    __tablename__ = "value"
+    id = Column('id', Integer, primary_key=True)
+    data  = Column("data", Float)
+    timestamp = Column("timestamp", DateTime(timezone=True))
+    belongs_to = Column(Integer, ForeignKey('box.id'), nullable= False)
+    measurement = Column(Integer, ForeignKey('measurement.id'), nullable= False)
+    
 if __name__ == "__main__":
     engine = create_engine("sqlite:///sensor.db", echo=True)
     Base.metadata.drop_all(bind=engine)
