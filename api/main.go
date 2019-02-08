dab74d15c81ef7802c20d3980b3efe6bde8c4181
Dan Amador <danamadorpe@gmail.com>
2019-02-08 15:38:04 +0100

    init

--- api/main.go
+++ api/main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  // Set the router as the default one shipped with Gin
-  router := gin.Default()
+	router := gin.Default()
+	sensors := new(Fuck.Sensor)
+	api := router.Group("/api")
+	{
+		api.GET("/", sensors.GetValues)
+	}
 
-  // Setup route group for the API
-  api := router.Group("/api")
-  {
-    api.GET("/", func(c *gin.Context) {
-      c.JSON(http.StatusOK, gin.H {
-        "message": "pong",
-      })
-    })
-  }
-
-  // Start and run the server
-  router.Run()
+	router.Run()
 }
