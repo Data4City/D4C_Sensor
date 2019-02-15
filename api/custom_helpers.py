092788b8f913b8c73423356596fe4e6f5e1b6f6d
Dan Amador <danamadorpe@gmail.com>
2019-02-15 16:53:00 +0100

    Separated decorators into multiple ones

--- api/custom_helpers.py
+++ api/custom_helpers.py
@@ -3,26 +3,35 @@ import falcon
 import jsonschema
 
 def validate(func, schema = None):
-        def wrapper(self, req, resp, *args, **kwargs):
+    def wrapper(self, req, resp, *args, **kwargs):
+        try:
+            obj = kwargs.get('parsed',req.stream.read(req.content_length or 0) )
+        except Exception as e:
+            raise falcon.HTTPBadRequest(
+                    'Failed data validation, no valid body',
+                    e.message
+                ) 
+        if(schema is not None and obj != ""):
             try:
-                data = req.stream.read(req.content_length or 0)
-                if len(data) == 0:
-                    obj = ""
-                else:
-                    obj = json.loads(data.decode('utf-8'))
-            except Exception:
+                jsonschema.validate(obj, schema)
+            except jsonschema.ValidationError as e:
                 raise falcon.HTTPBadRequest(
-                    'Invalid data',
-                    'Could not properly parse the provided data as JSON'
+                    'Failed data validation',
+                    e.message
                 )
+        return func(self, req, resp, *args, **kwargs)
+    return wrapper
 
-            if(schema is not None and obj != ""):
-                try:
-                    jsonschema.validate(obj, schema)
-                except jsonschema.ValidationError as e:
-                    raise falcon.HTTPBadRequest(
-                        'Failed data validation',
-                        e.message
-                    )
-            return func(self, req, resp, *args, parsed=obj, **kwargs)
-        return wrapper
+def get_json_body(func):
+    def wrapper(self, req, resp, *args, **kwargs):
+        try:
+            data = req.stream.read(req.content_length or 0)
+            if len(data) == 0:
+                obj = ""
+            else:
+                    obj = json.loads(data.decode('utf-8'))
+        except Exception:
+            raise falcon.HTTPBadRequest(
+                'Invalid data','Could not properly parse the provided data as JSON')
+        return func(self, req, resp, *args, parsed=obj, **kwargs)
+    return wrapper
