0591805e60161b6209f3c94f155a0ce943fa574f
Dan Amador <danamadorpe@gmail.com>
2019-02-28 15:50:19 +0100

    Changed git ignore and small refactors

--- api/routes.py
+++ api/routes.py
@@ -6,17 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-
-
-engine = create_engine("sqlite:///sensor.db", echo=True)
+engine = create_engine("sqlite:///sensor.db")#, echo=True)
 
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
+
 
 def get_app() -> API:
     _app = falcon.API(middleware=[SQLAlchemySessionManager(Session)])
     _app.add_route('/kit', KitResource())
     return _app
-
-
-
