7ae1f27bae88d3b795972726116ac0be017f0eb3
Dan Amador <danamadorpe@gmail.com>
2019-02-28 17:43:46 +0100

    Added db helper

--- api/custom_helpers.py
+++ api/custom_helpers.py
@@ -39,3 +39,14 @@ def get_json_body(func):
         return func(self, req, resp, *args, parsed=obj, **kwargs)
 
     return wrapper
+
+
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+        return instance
